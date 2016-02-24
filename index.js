c1255e6a974e567946e8895a80465e8e76f14c8c
Jérémy Lal <kapouer@melix.org>
2016-02-24 23:46:24 +0100

    Ability to build a page that has web components

--- index.js
+++ index.js
@@ -23,18 +23,27 @@ function WindowPage(inst) {
 	this.historyListener = this.historyListener.bind(this);
 	window.addEventListener('popstate', this.historyListener);
 
-	this.readyListener = this.readyListener.bind(this);
-	if (document.readyState == "interactive" || document.readyState == "complete") {
-		this.readyListener();
-	} else {
-		document.addEventListener('DOMContentLoaded', this.readyListener);
-	}
+	this.waitBuild(function() {
+		if (this.stage() == "build") {
+			this.builder = Promise.resolve();
+			this.waitHandle(this.handle.bind(this));
+		} else {
+			this.build();
+		}
+	}.bind(this));
 }
 
 WindowPage.prototype.build = function(fn) {
-	var phase = "building";
+	var phase = "build";
 	if (!fn) {
+		this.stage(phase);
 		this.builder = this.allFn(this.builds, phase);
+		var visible = document.documentElement.offsetWidth || document.documentElement.offsetHeight;
+		if (visible) {
+			this.builder.then(function() {
+				this.waitHandle(this.handle.bind(this));
+			}.bind(this));
+		}
 	} else {
 		this.builds.push(fn);
 		if (this.builder) this.builder = this.oneFn(this.builder, fn, phase);
@@ -44,7 +53,7 @@ WindowPage.prototype.build = function(fn) {
 
 
 WindowPage.prototype.handle = function(fn) {
-	var phase = "handling";
+	var phase = "handle";
 	if (!fn) {
 		this.handler = this.allFn(this.handles, phase);
 	} else {
@@ -75,35 +84,98 @@ WindowPage.prototype.allFn = function(list, phase) {
 	return p;
 };
 
-WindowPage.prototype.readyListener = function(e) {
-	document.removeEventListener('DOMContentLoaded', this.readyListener);
+WindowPage.prototype.stage = function(name) {
 	var root = document.documentElement;
-	var stage = root.getAttribute('stage');
+	if (name) root.setAttribute("stage", name);
+	else name = root.getAttribute("stage");
+	return name;
+};
 
-	if (!stage) {
-		stage = 'build';
-		root.setAttribute('stage', stage);
-		this.build();
-	} else if (stage == 'build') {
-		this.builder = Promise.resolve();
-	}
-
-	var visible = document.documentElement.offsetWidth || document.documentElement.offsetHeight;
-
-	if (stage != "ui" && visible) {
-		this.builder.then(function() {
-			root.setAttribute('stage', 'ui');
-			if (window.WebComponents) {
-				var wcListener = function() {
-					window.removeEventListener('WebComponentsReady', wcListener);
-					this.handle();
-				}.bind(this);
-				window.addEventListener('WebComponentsReady', wcListener);
-			} else {
-				this.handle();
+WindowPage.prototype.waitHandle = function(cb) {
+	Promise.all(Array.from(document.querySelectorAll('link[rel="import"]'))
+	.map(function(link) {
+		if (link.import) {
+			var state = link.import.readyState;
+			var isWebkit = window.navigator.userAgent.indexOf('AppleWebKit');
+			if (state == "complete" || (state == "loading" && isWebkit && window.WebComponents)) {
+				return Promise.resolve();
+		}
+		}
+		return new Promise(function(resolve, reject) {
+			function loadListener() {
+				link.removeEventListener('load', loadListener);
+				resolve();
 			}
-		}.bind(this));
+			function errorListener() {
+				link.removeEventListener('error', errorListener);
+				resolve();
+			}
+			link.addEventListener('load', loadListener);
+			link.addEventListener('error', errorListener);
+		});
+	})).then(function() {
+		cb();
+	});
+};
+
+WindowPage.prototype.waitBuild = function(cb) {
+	if (document.readyState == "interactive" || document.readyState == "complete") {
+		cb();
+	} else {
+		function listener() {
+			document.removeEventListener('DOMContentLoaded', listener);
+			cb();
+		}
+		document.addEventListener('DOMContentLoaded', listener);
 	}
+};
+
+WindowPage.prototype.import = function(doc) {
+	var scripts = Array.from(doc.querySelectorAll('script')).map(function(node) {
+		if (node.type && node.type != "text/javascript") return Promise.resolve();
+		// make sure script is not loaded when inserted into document
+		node.type = "text/plain";
+		// fetch script content ourselves
+		if (node.src) return GET(node.src).then(function(txt) {
+			return {src: node.src, txt: txt, node: node};
+		}).catch(function(err) {
+			console.error("Error loading", node.src, err);
+		});
+		else return Promise.resolve({
+			src: "inline", txt: node.textContent, node: node
+		});
+	});
+
+	var imports = Array.from(doc.querySelectorAll('link[rel="import"]'));
+	imports.forEach(function(link) {
+		link.rel = "";
+	});
+
+	var root = document.documentElement;
+	for (var i=0; i < doc.attributes.length; i++) {
+		root.setAttribute(doc.attributes[i].name, doc.attributes[i].value);
+	}
+	root.replaceChild(document.adoptNode(doc.head), document.head);
+	root.replaceChild(document.adoptNode(doc.body), document.body);
+
+	// execute all scripts at once in their original order
+	return Promise.all(scripts).then(function(list) {
+		list.forEach(function(obj) {
+			if (!obj.txt) return;
+			var script = document.createElement("script");
+			script.textContent = obj.txt;
+			document.head.appendChild(script).remove();
+			obj.node.type = "text/javascript";
+		});
+		imports.forEach(function(link) {
+			var after = link.nextSibling;
+			var parent = link.parentNode;
+			link.remove();
+			link.rel = "import";
+			if (after) parent.insertBefore(link, after);
+			else parent.appendChild(link);
+		});
+	});
 };
 
 /*
