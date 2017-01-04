2c0033fe752abf1834fcf867aaf9444f04696517
Jérémy Lal <kapouer@melix.org>
2017-01-04 02:03:08 +0100

    Test basic prerender on server then render on client

---
+++ test/prerender-render.js
@@ -0,0 +1,64 @@
+var expect = require('expect.js');
+var request = require('request');
+var express = require('express');
+
+var host = "http://localhost";
+var dom = require('express-dom');
+
+dom.settings.stall = 5000;
+dom.settings.allow = 'all';
+dom.settings.timeout = 10000;
+dom.settings.console = true;
+
+var renderPlugins = [
+	dom.plugins.hide,
+	dom.plugins.nomedia,
+	dom.plugins.redirect,
+	dom.plugins.referrer,
+	dom.plugins.html
+];
+
+describe("Two-phase rendering", function suite() {
+	this.timeout(3000);
+	var server, port;
+
+	before(function(done) {
+		var app = express();
+		app.set('views', __dirname + '/public');
+		app.get(/\.(json|js|css|png)$/, express.static(app.get('views')));
+		app.get(/\.html$/, dom().load());
+
+
+		server = app.listen(function(err) {
+			if (err) console.error(err);
+			port = server.address().port;
+			done();
+		});
+	});
+
+	after(function(done) {
+		server.close();
+		done();
+	});
+
+
+	it("should run build then setup", function(done) {
+		var buildUrl = host + ':' + port + '/build.html';
+		request({
+			method: 'GET',
+			url: buildUrl
+		}, function(err, res, body) {
+			expect(res.statusCode).to.be(200);
+			expect(body.indexOf("I'm built")).to.be.greaterThan(0);
+			dom(body).load({
+				plugins: renderPlugins
+			})(buildUrl).then(function(state) {
+				expect(state.body.indexOf("I'm setup")).to.be.greaterThan(0);
+				done();
+			});
+		});
+	});
+
+
+});
+
