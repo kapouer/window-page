b6b4113259febcf3f0a66f4552d2dd2b2caa4c8e
Jérémy Lal <kapouer@melix.org>
2017-01-04 01:18:36 +0100

    Basic render tests

---
+++ test/render.js
@@ -0,0 +1,67 @@
+var expect = require('expect.js');
+var request = require('request');
+var express = require('express');
+
+var host = "http://localhost";
+var dom = require('express-dom');
+
+dom.settings.stall = 5000;
+dom.settings.allow = 'all';
+dom.settings.timeout = 10000;
+dom.settings.console = true;
+
+dom.settings.load.plugins = [
+	dom.plugins.hide,
+	dom.plugins.redirect,
+	dom.plugins.referrer,
+	dom.plugins.html
+];
+
+describe("Rendering", function suite() {
+	this.timeout(3000);
+	var server, port;
+
+	before(function(done) {
+		var app = express();
+		app.set('views', __dirname + '/public');
+		app.get(/\.(json|js|css|png)$/, express.static(app.get('views')));
+		app.get(/\.html$/, dom().load());
+
+
+		server = app.listen(function(err) {
+			if (err) console.error(err);
+			port = server.address().port;
+			done();
+		});
+	});
+
+	after(function(done) {
+		server.close();
+		done();
+	});
+
+
+	it("should run build and setup", function(done) {
+		request({
+			method: 'GET',
+			url: host + ':' + port + '/build.html'
+		}, function(err, res, body) {
+			expect(res.statusCode).to.be(200);
+			expect(body.indexOf("I'm setup")).to.be.greaterThan(0);
+			done();
+		});
+	});
+
+	it("should run route and build and setup", function(done) {
+		request({
+			method: 'GET',
+			url: host + ':' + port + '/route.html'
+		}, function(err, res, body) {
+			expect(res.statusCode).to.be(200);
+			expect(body.indexOf("I'm setup")).to.be.greaterThan(0);
+			done();
+		});
+	});
+
+});
+
