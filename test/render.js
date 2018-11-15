82313e452d0dace13d46260e53a719c5cb35c257
Jérémy Lal <kapouer@melix.org>
2018-11-15 13:26:51 +0100

    More tests and fix some of them. Render tests support DEBUG=1 env.

--- test/render.js
+++ test/render.js
@@ -17,10 +17,44 @@ dom.settings.stallTimeout = 1000;
 dom.settings.console = true;
 
 var loadPlugins = [
+	debugPlugin,
 	dom.plugins.redirect,
 	dom.plugins.referrer,
 	dom.plugins.html
 ];
+
+function debugPlugin(page, settings) {
+	if (process.env.DEBUG) {
+		if (!settings.scripts) settings.scripts = [];
+		settings.scripts.push(function(n) {
+			window.debug = console.error.bind(console);
+		});
+	}
+}
+
+function Render(url, opts) {
+	if (!opts) opts = {};
+	return new Promise(function(resolve, reject) {
+		Web(function(err, page) {
+			var settings = {
+				stallTimeout: 1000,
+				console: true,
+				navigation: true
+			};
+			debugPlugin(page, settings);
+
+			page.when('ready', function() {
+				if (opts.ready) page.run(opts.ready);
+			});
+			page.when('idle', function() {
+				setTimeout(function() {
+					page.html().then(resolve);
+				}, opts.delay);
+			}).catch(reject);
+			page.load(url, settings);
+		});
+	});
+}
 
 var host = "http://localhost";
 
@@ -76,42 +110,24 @@ describe("Rendering", function suite() {
 		});
 	});
 
-	it("should run build and patch and setup, call replace and run patch again", function(done) {
-		Web(function(err, page) {
-			page.load(host + ':' + port + '/replace.html', {
-				stallTimeout: 1000,
-				console: true,
-				navigation: true
-			}).when('idle', function() {
-				return page.html().then(function(body) {
-					expect(body).to.contain('data-page-stage="setup"');
-					expect(body).to.contain('<div class="build">1</div>');
-					expect(body).to.contain('<div class="patch">2</div>');
-					expect(body).to.contain('<div class="setup">1</div>');
-					expect(body).to.contain('<div class="url">/replace.html?toto=1</div>');
-					done();
-				});
-			}).catch(done);
+	it("should run build and patch and setup, call replace and run patch again", function() {
+		return Render(host + ':' + port + '/templates/replace.html', {delay: 150}).then(function(body) {
+			expect(body).to.contain('data-page-stage="setup"');
+			expect(body).to.contain('<div class="build">1</div>');
+			expect(body).to.contain('<div class="patch">2</div>');
+			expect(body).to.contain('<div class="setup">1</div>');
+			expect(body).to.contain('<div class="url">/templates/replace.html?toto=1</div>');
 		});
 	});
 
-	it("should run build and patch and setup, call push and run patch again", function(done) {
-		Web(function(err, page) {
-			page.load(host + ':' + port + '/push.html', {
-				stallTimeout: 1000,
-				console: true,
-				navigation: true
-			}).when('idle', function() {
-				return page.html().then(function(body) {
-				expect(body).to.contain('data-page-stage="setup"');
-				expect(body).to.contain('<div class="build">1</div>');
-				expect(body).to.contain('<div class="patch">3</div>');
-				expect(body).to.contain('<div class="setup">1</div>');
-				expect(body).to.contain('<div class="url">/push.html?toto=2</div>');
-				expect(body).to.contain('<div class="location">/push.html?toto=2</div>');
-					done();
-				});
-			}).catch(done);
+	it("should run build and patch and setup, call push and run patch again", function() {
+		return Render(host + ':' + port + '/templates/push.html', {delay: 150}).then(function(body) {
+			expect(body).to.contain('data-page-stage="setup"');
+			expect(body).to.contain('<div class="build">1</div>');
+			expect(body).to.contain('<div class="patch">3</div>');
+			expect(body).to.contain('<div class="setup">1</div>');
+			expect(body).to.contain('<div class="url">/templates/push.html?toto=2</div>');
+			expect(body).to.contain('<div class="location">/templates/push.html?toto=2</div>');
 		});
 	});
 
@@ -178,91 +194,63 @@ describe("Rendering", function suite() {
 		});
 	});
 
-	it("should run build and patch then setup then back then build", function(done) {
-		Web(function(err, page) {
-			page.load(host + ':' + port + '/templates/back-build.html', {
-				stallTimeout: 100,
-				console: true,
-				navigation: true
-			}).when('ready', function() {
-				page.run(function(cb) {
-					try {
-						window.testcb = cb;
-						var script = document.createElement('script');
-						script.textContent = `
-						Page.route(function(state) {
-							if (state.pathname == "/inexistent.html") setTimeout(function() {
-								window.simClick();
-								window.testcb();
-							}, 250);
-						});`;
-						document.head.appendChild(script);
-					} catch(ex) {
-						cb(ex);
-					}
-				}).then(function() {
-					return page.html().then(function(body) {
-						expect(body).to.contain('<div class="build">2</div>');
-						expect(body).to.contain('<div class="setup">2</div>');
-						expect(body).to.contain('<div id="click">1</div>');
-						done();
-					});
-				}).catch(function(err) {
-					done(err);
-				});
-			}).catch(done);
+	it("should run build and patch then setup then back then build", function() {
+		return Render(host + ':' + port + '/templates/back-build.html', {
+			ready: function(cb) {
+				window.testcb = cb;
+				try {
+					var script = document.createElement('script');
+					script.textContent = `
+					Page.route(function(state) {
+						if (state.pathname == "/inexistent.html") setTimeout(function() {
+							window.simClick();
+							window.testcb();
+						}, 250);
+					});`;
+					document.head.appendChild(script);
+				} catch(ex) {
+					console.error(ex);
+				}
+			}
+		}).then(function(body) {
+			expect(body).to.contain('<div class="build">2</div>');
+			expect(body).to.contain('<div class="setup">1</div>');
+			expect(body).to.contain('<div id="click">1</div>');
+			expect(body).to.contain('<div id="nocall"></div>');
 		});
 	});
 
-	it("should parse state.hash and run hash chain on click", function(done) {
-		Web(function(err, page) {
-			page.load(host + ':' + port + '/templates/hash-click.html#test', {
-				stallTimeout: 1000,
-				console: true,
-				navigation: true
-			}).when('idle', function() {
-				return page.html().then(function(body) {
-					expect(body).to.contain('data-page-stage="setup"');
-					expect(body).to.contain('<div class="hash">test</div>');
-					expect(body).to.contain('<div class="secondhash">toto</div>');
-					done();
-				});
-			}).catch(done);
+	it("should parse state.hash and run hash chain on click", function() {
+		return Render(host + ':' + port + '/templates/hash-click.html#test').then(function(body) {
+			expect(body).to.contain('data-page-stage="setup"');
+			expect(body).to.contain('<div class="hash">test</div>');
+			expect(body).to.contain('<div class="secondhash">toto</div>');
 		});
 	});
 
-	it("should run hash chain on push", function(done) {
-		Web(function(err, page) {
-			page.load(host + ':' + port + '/templates/hash-push.html', {
-				stallTimeout: 1000,
-				console: true,
-				navigation: true
-			}).when('idle', function() {
-				return page.html().then(function(body) {
-					expect(body).to.contain('data-page-stage="setup"');
-					expect(body).to.contain('<div class="hash">test</div>');
-					done();
-				});
-			}).catch(done);
+	it("should run hash chain on push", function() {
+		return Render(host + ':' + port + '/templates/hash-push.html').then(function(body) {
+			expect(body).to.contain('data-page-stage="setup"');
+			expect(body).to.contain('<div class="hash">test</div>');
 		});
 	});
 
-	it("should allow early setup on state.emitter but not twice for same function", function(done) {
-		Web(function(err, page) {
-			page.load(host + ':' + port + '/templates/early-setup.html', {
-				stallTimeout: 1000,
-				console: true,
-				navigation: true
-			}).when('idle', function() {
-				setTimeout(function() {
-					return page.html().then(function(body) {
-						expect(body).to.contain('data-page-stage="setup"');
-						expect(body).to.contain('<div class="testA">1</div>');
-						expect(body).to.contain('<div class="testB">1</div>');
-						done();
-					}).catch(done);
-				}, 150);
-			}).catch(done);
+	it("should allow early setup on state.emitter but not twice for same function", function() {
+		return Render(host + ':' + port + '/templates/early-setup.html').then(function(body) {
+			expect(body).to.contain('data-page-stage="setup"');
+			expect(body).to.contain('<div class="testA">1</div>');
+			expect(body).to.contain('<div class="testB">1</div>');
+		});
+	});
+
+	it("should route setup and close and reload", function() {
+		return Render(host + ':' + port + '/templates/reload.html?template=reload-helper', {
+			delay: 250
+		}).then(function(body) {
+			expect(body).to.contain('data-setup="2"');
+			expect(body).to.contain('data-close="1"');
+			expect(body).to.contain('data-page-stage="setup"');
+			expect(body).to.contain('<div id="reload">reloaded</div>');
 		});
 	});
 });
