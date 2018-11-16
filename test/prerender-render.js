4bc8c84d3fe644f042768532776def67710f314c
Jérémy Lal <kapouer@melix.org>
2018-11-16 09:02:07 +0100

    Port Render function to other prerender-render

--- test/prerender-render.js
+++ test/prerender-render.js
@@ -15,10 +15,44 @@ dom.settings.timeout = 10000;
 dom.settings.console = true;
 
 var renderPlugins = [
+	debugPlugin,
 	dom.plugins.redirect,
 	dom.plugins.referrer,
 	dom.plugins.html
 ];
+
+function debugPlugin(page, settings) {
+	if (process.env.DEBUG) {
+		if (!settings.scripts) settings.scripts = [];
+		settings.scripts.push(function(n) {
+			window.debug = console.error.bind(console);
+		});
+	}
+}
+
+function Render(url, opts) {
+	if (!opts) opts = {};
+	return new Promise(function(resolve, reject) {
+		Web(function(err, page) {
+			var settings = {
+				stallTimeout: 1000,
+				console: true,
+				navigation: true
+			};
+			debugPlugin(page, settings);
+
+			page.when('ready', function() {
+				if (opts.ready) page.run(opts.ready);
+			});
+			page.when('idle', function() {
+				setTimeout(function() {
+					page.html().then(resolve);
+				}, opts.delay);
+			}).catch(reject);
+			page.load(url, settings);
+		});
+	});
+}
 
 var host = "http://localhost";
 
@@ -165,44 +199,22 @@ describe("Two-phase rendering", function suite() {
 		});
 	});
 
-	it("should run build and patch then setup then patch then back then patch", function(done) {
-		Web.load(host + ':' + port + '/back-patch.html', {
-			stallTimeout: 100,
-			console: true,
-			navigation: true
-		}).once('idle', function() {
-			setTimeout(function() {
-				this.html().then(function(body) {
-					expect(body).to.contain('<div class="build">1</div>');
-					expect(body).to.contain('<div class="patch">2</div>');
-					expect(body).to.contain('<div class="setup">1</div>');
-					expect(body).to.contain('<div id="loc">/back-patch.html?test=one</div>');
-					expect(body).to.contain('<div id="back">/back-patch.html</div>');
-					done();
-				}).catch(function(err) {
-					done(err);
-				});
-			}.bind(this), 500);
+	it("should run build and patch then setup then patch then back then patch", function() {
+		return Render(host + ':' + port + '/back-patch.html', {delay: 500}).then(function(body) {
+			expect(body).to.contain('<div class="build">1</div>');
+			expect(body).to.contain('<div class="patch">2</div>');
+			expect(body).to.contain('<div class="setup">1</div>');
+			expect(body).to.contain('<div id="loc">/back-patch.html?test=one</div>');
+			expect(body).to.contain('<div id="back">/back-patch.html</div>');
 		});
 	});
 
-	it("should run build then setup then hash", function(done) {
-		Web.load(host + ':' + port + '/hash.html#test', {
-			stallTimeout: 100,
-			console: true,
-			navigation: true
-		}).once('idle', function() {
-			setTimeout(function() {
-				this.html().then(function(body) {
-					expect(body).to.contain('<div class="build">1</div>');
-					expect(body).to.contain('<div class="setup">1</div>');
-					expect(body).to.contain('<div class="hash">1</div>');
-					expect(body).to.contain('<div id="hash">test</div>');
-					done();
-				}).catch(function(err) {
-					done(err);
-				});
-			}.bind(this), 500);
+	it("should run build then setup then hash", function() {
+		return Render(host + ':' + port + '/hash.html#test', {delay: 500}).then(function(body) {
+			expect(body).to.contain('<div class="build">1</div>');
+			expect(body).to.contain('<div class="setup">1</div>');
+			expect(body).to.contain('<div class="hash">1</div>');
+			expect(body).to.contain('<div id="hash">test</div>');
 		});
 	});
 });
