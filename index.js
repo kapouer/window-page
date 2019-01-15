ecae01e7d2170dcac682de8bf22b6729f65af6b5
Jérémy Lal <kapouer@melix.org>
2019-01-15 16:11:30 +0100

    Do not do anything if window.Page is already loaded

--- index.js
+++ index.js
@@ -2,19 +2,20 @@ var Utils = require('./src/utils');
 var Loc = require('./src/loc');
 var State = require('./src/state');
 
-var W = State.Page = window.Page = {};
+if (!window.Page) {
+	var W = State.Page = window.Page = {};
 
-Object.assign(W, Loc);
-W.createDoc = Utils.createDoc;
-W.get = Utils.get;
+	Object.assign(W, Loc);
+	W.createDoc = Utils.createDoc;
+	W.get = Utils.get;
 
-W.route = function(fn) {
-	State.prototype.router = function() {
-		return fn(this);
+	W.route = function(fn) {
+		State.prototype.router = function() {
+			return fn(this);
+		};
 	};
-};
 
-Loc.parse().run().then(function(state) {
-	if (!window.history.state) state.save();
-});
-
+	Loc.parse().run().then(function(state) {
+		if (!window.history.state) state.save();
+	});
+}
