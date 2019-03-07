a31f560e2359435aab9d29adb0389e027b7a3a82
Jérémy Lal <kapouer@melix.org>
2019-03-08 00:23:56 +0100

    Do not short-cut checks when queuing

--- src/state.js
+++ src/state.js
@@ -84,13 +84,8 @@ State.prototype.init = function() {
 State.prototype.run = function() {
 	var state = this;
 	if (queue) {
-		queue = queue.then(function(old) {
-			if (Loc.samePath(state, old)) {
-				queue = null;
-				return old;
-			} else {
-				return state.run();
-			}
+		queue = queue.then(function() {
+			return state.run();
 		});
 	} else {
 		queue = run(state).then(function(state) {
