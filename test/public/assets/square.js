706f9a0c7046c16b15566aff9d52360407f75972
Jérémy Lal <kapouer@melix.org>
2019-12-19 10:39:57 +0100

    Use Page.setup in that test

--- test/public/assets/square.js
+++ test/public/assets/square.js
@@ -1,15 +1,8 @@
-document.addEventListener('DOMContentLoaded', function() {
+Page.setup(function() {
 	if (document.body.offsetWidth == 400) {
 		document.querySelector('.status').innerHTML = "squared";
 	} else {
 		document.querySelector('.status').innerHTML = "not";
 	}
 });
-if (document.body) {
-	if (document.body.offsetWidth == 400) {
-		document.querySelector('.status').innerHTML = "squared";
-	} else {
-		document.querySelector('.status').innerHTML = "not";
-	}
-}
 
