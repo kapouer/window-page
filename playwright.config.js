d55dacfdfe49ad6e58b6f3f1940a0b89e7f77df6
Jérémy Lal <kapouer@melix.org>
2024-10-31 11:24:21 +0100

    Fix playwright config

--- playwright.config.js
+++ playwright.config.js
@@ -1,10 +1,15 @@
-module.exports = {
+const { defineConfig } = require('@playwright/test');
+
+module.exports = defineConfig({
 	timeout: 10000,
 	use: {
 		browserName: 'chromium',
-		channel: 'chrome'
+		channel: 'chrome',
+		launchOptions: {
+			executablePath: '/usr/bin/chromium'
+		}
 	},
 	expect: {
 		timeout: 1 // make expect pooling fail
 	}
-};
+});
